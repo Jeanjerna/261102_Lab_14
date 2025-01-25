17cd7e30993df4462d5f90ca96c5dcc57ec900d9
Jeanjerna <chaichanchaisak2549@gmail.com>
2025-01-25 11:47:21 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,35 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	double x;
+    int j;
+    
+    for (int i = 1 ;  i < N ; i++)
+    {
+        x = d[i];
+        
+        for (j = i-1 ; j >= 0 ; j--)
+        {
+            if ( x > d[j])
+            {
+                d[j+1] = d[j];
+                // if (j == 0) d[j] = x;
+            }
+            else break;
+        }
+        d[j+1] = x; //ถ้าเช็คจนถึงตัวที่ 0 แล้ว x ยังมากกว่า d[j] จะไม่มีการ break เกิดขึ้นแล้วทำให้ค่าสุดท้ายของ j = -1 ทำให้ d[j+1] = d[0]
+        
+        //====================================//
+        cout << "Pass " << i << ":";
+        
+        for (int l = 0 ; l < N ; l++)
+        {
+            cout << d[l] << " ";
+        }
+        
+        cout << "\n";
+        //====================================//
+    }
 }
 
 int main(){
